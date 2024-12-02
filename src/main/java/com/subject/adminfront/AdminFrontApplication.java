fb9a09266856048cad6e9bdd058571bd32853888
박종국 <whdrnrdl789@naver.com>
2024-12-02 23:12:53 +0900

    first init

---
+++ src/main/java/com/subject/adminfront/AdminFrontApplication.java
@@ -0,0 +1,13 @@
+package com.subject.adminfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/subject/adminfront/AdminFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subject.adminfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
